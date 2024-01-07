9c3574a374ac1a9d0e35b26136a97c76be2c944c
Leonardo Tandeli <leonardotandelicw@outlook.com>
2024-01-07 09:05:59 -0300

    feat: add socialnetwork url to variables

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,6 +8,9 @@ import { GithubIcon, LinkedinIcon } from 'lucide-react';
 import SocialNetworkButton from '../socialNetworkButton/SocialNetworkButton';
 
 export default function Header(): JSX.Element {
+  const LINKEDIN_URL = 'https://www.linkedin.com/in/leonardotandeli';
+  const GITHUB_URL = 'https://github.com/leonardotandeli';
+  
   return (
     <header
       className={`background flex items-center justify-center md:justify-between  py-4 flex-wrap gap-4 md:gap-0`}
@@ -16,11 +19,11 @@ export default function Header(): JSX.Element {
 
       <div className={`flex flex-row justify-center items-center space-x-8 `}>
         <div className={`flex flex-row justify-center items-center space-x-2 `}>
-          <SocialNetworkButton url="https://www.linkedin.com/in/leonardotandeli">
+          <SocialNetworkButton url={LINKEDIN_URL}>
             <LinkedinIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:text-blue-500" />
           </SocialNetworkButton>
 
-          <SocialNetworkButton url="https://github.com/leonardotandeli">
+          <SocialNetworkButton url={GITHUB_URL}>
             <GithubIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:text-blue-500" />
           </SocialNetworkButton>
         </div>
