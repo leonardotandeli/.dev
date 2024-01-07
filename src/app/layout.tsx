520643d5bd4871f4e34bbe1c10c024ac4d8ac5a5
Leonardo Tandeli <leonardotandelicw@outlook.com>
2024-01-07 01:51:07 -0300

    feat: add providers from next-themes to allow dark-mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,26 @@
-'use client'
-import './globals.css'
-import { ThemeProvider } from 'next-themes'
+import Header from '@/components/header/Header';
+import './globals.css';
+
+import Footer from '@/components/footer/Footer';
+import Providers from '@/contexts/Providers';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }): JSX.Element {
   return (
     <>
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  </>
-  )
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body className="container">
+          <Providers>
+            <Header />
+            {children}
+            <Footer />
+          </Providers>
+        </body>
+      </html>
+    </>
+  );
 }
